cf897b3050eb7cad229a4d783d5a317e7074ee61
justclaner <justclaner@gmail.com>
2025-10-23 01:04:57 -0400

    added boilerplate

---
+++ auth.js
@@ -0,0 +1,19 @@
+const allowCors = (req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', 'https://flashcards-seven-nu.vercel.app') //change this
+    //'https://flashcards-seven-nu.vercel.app' when not testing in localhost
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,DELETE,POST,PUT,OPTIONS')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+      }
+    next();
+  }
+ 
+  export default allowCors;
+  
+  
